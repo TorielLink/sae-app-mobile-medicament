8c4205521411332b42222b252edbc4151f1d8325
Rémi <contact@remilemaire.fr>
2024-02-15 11:51:38 +0100

    MAJ dependencies + add nativeWind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.js", "./screens/**/*.js", "./components/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
